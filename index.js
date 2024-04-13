080449331c69a0f4026e21f3c042e56cba15c22b
Railton Dantas <155496471+RailtonDantas@users.noreply.github.com>
2024-04-13 15:20:48 -0300

    adição de mais um tipo de erro

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ btnSubmit.addEventListener("click", () => {
 
 
 const ctx = document.getElementById('myChart');
-let str = 30/100
 
 Chart.defaults.color = 'white';
 Chart.defaults.font.weight = 'bolder'
@@ -36,13 +35,13 @@ Chart.defaults.font.family = 'Helvetica,sans-serif'
 new Chart(ctx, { 
   type: 'doughnut',
   data: {
-    labels: ['ATENÇÃO', 'TEORIA','INTERPRETAÇÃO','SACADA','TEMPO','RACIOCÍNIO CERTO MAS TRAVOU','ANÁLISE DE GRÁFICO'],
+    labels: ['ATENÇÃO', 'TEORIA','INTERPRETAÇÃO','SACADA','TEMPO','RACIOCÍNIO CERTO MAS TRAVOU','ANÁLISE DE GRÁFICO','FICOU EM DÚVIDA E MARCOU A ERRADA'],
     datasets: [{
       label:`#Quantidade de erros `,
-      data: [localStorage.getItem('typeErro0'),localStorage.getItem('typeErro1'),localStorage.getItem('typeErro2'),localStorage.getItem('typeErro3'),localStorage.getItem('typeErro4'),localStorage.getItem('typeErro5'),localStorage.getItem('typeErro6')],
+      data: [localStorage.getItem('typeErro0'),localStorage.getItem('typeErro1'),localStorage.getItem('typeErro2'),localStorage.getItem('typeErro3'),localStorage.getItem('typeErro4'),localStorage.getItem('typeErro5'),localStorage.getItem('typeErro6'),localStorage.getItem('typeErro7')],
       borderWidth: 1,
       borderColor:'rgba(0,0,0,0.5)',
-      backgroundColor:['#203500','#283e06','#31470b','#3b5110','#445a14','#577e03','#3f4b25'],
+      backgroundColor:['#203500','#283e06','#31470b','#3b5110','#445a14','#577e03','#3f4b25','#192204'],
       color:['black'],
     }]
   },
