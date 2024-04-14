f178a2f2e413ad64e821dfe5b58754d7017518d8
Railton Dantas <155496471+RailtonDantas@users.noreply.github.com>
2024-04-14 12:06:25 -0300

    update

--- index.js
+++ index.js
@@ -26,24 +26,26 @@ btnSubmit.addEventListener("click", () => {
 
 
 
+
 const ctx = document.getElementById('myChart');
 
-Chart.defaults.color = 'white';
+Chart.defaults.color = 'black';
 Chart.defaults.font.weight = 'bolder'
 Chart.defaults.font.family = 'Helvetica,sans-serif'
 
+
 new Chart(ctx, { 
-  type: 'doughnut',
+  type: 'pie',
   data: {
     labels: ['ATENÇÃO', 'TEORIA','INTERPRETAÇÃO','SACADA','TEMPO','RACIOCÍNIO CERTO MAS TRAVOU','ANÁLISE DE GRÁFICO','FICOU EM DÚVIDA E MARCOU A ERRADA'],
     datasets: [{
       label:`#Quantidade de erros `,
       data: [localStorage.getItem('typeErro0'),localStorage.getItem('typeErro1'),localStorage.getItem('typeErro2'),localStorage.getItem('typeErro3'),localStorage.getItem('typeErro4'),localStorage.getItem('typeErro5'),localStorage.getItem('typeErro6'),localStorage.getItem('typeErro7')],
       borderWidth: 1,
-      borderColor:'rgba(0,0,0,0.5)',
-      backgroundColor:['#203500','#283e06','#31470b','#3b5110','#445a14','#577e03','#3f4b25','#192204'],
-      color:['black'],
-    }]
+      borderColor:'rgb(255,255,255)',
+      backgroundColor:['#32666B','#33426C','#4E81A6','#254E45','#706ADD','#B4CEE0','#56636C','#5EE1C5'],    
+    }
+  ],
   },
   options: {
  
