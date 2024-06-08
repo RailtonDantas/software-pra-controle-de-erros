012ea9dc5f11b509cbfe3b041a2a4cf226a82b4b
Railton Dantas <155496471+RailtonDantas@users.noreply.github.com>
2024-06-08 15:11:51 -0300

    add new type error

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const paragraphs = [...document.querySelectorAll(".percentItem > p > span")]
 const select = document.querySelector("select")
 const [btn_showGraphics,btnSubmit,btn_showPercent] = document.querySelectorAll("#containerButtons > button") 
 let sum = 0
-select.style.opacity
 
 
 if(!localStorage.key('typeGraphic')){
@@ -69,13 +68,13 @@ Chart.defaults.font.family = 'Helvetica,sans-serif'
 new Chart(ctx, { 
   type: localStorage.getItem('typeGraphic'),
   data: {
-    labels: ['ATENÇÃO', 'TEORIA','INTERPRETAÇÃO','SACADA','TEMPO','RACIOCÍNIO CERTO MAS TRAVOU','ANÁLISE DE GRÁFICO','FICOU EM DÚVIDA E MARCOU A ERRADA'],
+    labels: ['ATENÇÃO', 'TEORIA','INTERPRETAÇÃO','SACADA','TEMPO','RACIOCÍNIO CERTO MAS TRAVOU','ANÁLISE DE GRÁFICO','FICOU EM DÚVIDA E MARCOU A ERRADA','EXTRAPOLAR'],
     datasets: [{
       label:`#Quantidade de erros `,
-      data: [localStorage.getItem('typeErro0'),localStorage.getItem('typeErro1'),localStorage.getItem('typeErro2'),localStorage.getItem('typeErro3'),localStorage.getItem('typeErro4'),localStorage.getItem('typeErro5'),localStorage.getItem('typeErro6'),localStorage.getItem('typeErro7')],
+      data: [localStorage.getItem('typeErro0'),localStorage.getItem('typeErro1'),localStorage.getItem('typeErro2'),localStorage.getItem('typeErro3'),localStorage.getItem('typeErro4'),localStorage.getItem('typeErro5'),localStorage.getItem('typeErro6'),localStorage.getItem('typeErro7'),localStorage.getItem('typeErro8')],
       borderWidth: 1,
       borderColor:'rgb(255,255,255)',
-      backgroundColor:['#32666B','#33426C','#4E81A6','#254E45','#706ADD','#B4CEE0','#56636C','#5EE1C5'],    
+      backgroundColor:['#32666B','#33426C','#4E81A6','#254E45','#706ADD','#B4CEE0','#56636C','#5EE1C5','#00ffff'],    
     }
   ],
   },
